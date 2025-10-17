a7c2ef1f6eea705361a484be556db5081d95003c
JAHNAVI-YERRAMSETTI <2300031078@kluniversity.in>
2025-10-17 13:36:56 +0530

    updaed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MainNavBar from './main/MainNavBar';
 import AddContact from './contact/AddContact';
 import ViewAllContacts from './contact/ViewAllContacts';
@@ -10,20 +10,15 @@ import './main/style.css';
 export default function App() {
   return (
     <>
-    <h2 className="cms-title" >Contact Management</h2>
-    <BrowserRouter>
+      <h2 className="cms-title">Contact Management</h2>
       <MainNavBar />
       <Routes>
-        
         <Route path="/add-contact" element={<AddContact />} />
         <Route path="/view-contacts" element={<ViewAllContacts />} />
         <Route path="/view-contact" element={<ViewContactById />} />
         <Route path="/update-contact" element={<UpdateContact />} />
         <Route path="/delete-contact" element={<DeleteContact />} />
-        <Route path="*" element={<div>Page Not Found</div>} />
       </Routes>
-    </BrowserRouter>
     </>
   );
-  
 }
